a6e07bf2dede835f713b5f5e0ea3a1d8bbcc5938
Sofia <sofiamino78@gmail.com>
2024-01-12 17:05:29 -0300

    blog app

---
+++ 11blogapp/src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {useDispatch} from 'react-redux'
+import authServive from '../../appwrite/auth.js'
+import {logout} from '../../store/authSlice.js'
+
+
+function LogoutBtn() {
+    const dispatch = useDispatch()
+
+    const logoutHandler = () => {
+        authServive.logout().then(() => {
+            dispatch(logout());
+        })
+    }
+  return (
+    <button
+        className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+        onClick={logoutHandler}
+    >
+        Logout
+    </button>
+  )
+}
+
+export default LogoutBtn
---
+++ 11blogapp/src/pages/AllPosts.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import appwriteService from '../appwrite/config'
+import Container from '../components/container/Container'
+import PostCard from '../components/PostCard'
+
+function AllPosts() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    appwriteService.getPosts([]).then((post) => {
+      if (posts) {
+        setPosts(posts.documents)
+      }
+    })
+  }, [])
+  
+  return (
+    <div className='w-full py-8'>
+      <Container>
+        <div className='flex flex-wrap'>
+          {posts.map((post) => (
+            <div className='p-2 w-1/4' key={post.$id}>
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default AllPosts
